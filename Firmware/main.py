4ea2fab221452f96a454cf97dd71c4ef4ff8ba28
Piyal Saha <125718339+piVerified@users.noreply.github.com>
2026-03-19 18:43:22 +0530

    added base formware

--- Firmware/main.py
+++ Firmware/main.py
@@ -1,13 +1,12 @@
-print("Starting PI.KEEB")
+print("Starting PI.KEEBV2")
 
 import board
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.matrix import DiodeOrientation
 from kmk.handlers.sequences import send_string
-from kmk.modules.encoder import EncoderHandler
 from kmk.modules.layers import Layers
-from kmk.extensions.peg_oled_display import Oled, OledDisplayMode, OledReactionType
+
 
 keyboard = KMKKeyboard()
 
@@ -16,40 +15,19 @@ keyboard.col_pins = (board.GP26, board.GP27, board.GP28)
 keyboard.row_pins = (board.GP2, board.GP3, board.GP4)
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 
-#modules
-layers = Layers()
-encoder_handler = EncoderHandler()
-keyboard.modules = [layers, encoder_handler]
 
-#Macros
-SPOTIFY = send_string(KC.LGUI(KC.S), "spotify", KC.ENTER)
-EDGE = send_string(KC.LGUI(KC.S), "edge", KC.ENTER)
-CALENDAR = KC.LGUI(KC.N)
+#Macros TBD
+
 
 #Keymap 
 keyboard.keymap = [
     [
-        KC.LCTL(KC.C), KC.LCTL(KC.V), KC.CALC,
-        CALENDAR,      SPOTIFY,       KC.NO,
-        EDGE,          KC.NO,         KC.MUTE
+        KC.1, KC.2, KC.3,
+        KC.4, KC.5, KC.6,
+        KC.7, KC.8, KC.9,
+
     ]
-]
 
-#Encoder 
-encoder_handler.pins = ((board.GP0, board.GP1, None, False),)
-encoder_handler.map = [
-    ((KC.VOLU, KC.VOLD),)
-]
-
-#OLED 
-oled_ext = Oled(
-    OledDisplayMode.STATIC,
-    oob_config={
-        'scl': board.GP7,
-        'sda': board.GP6,
-    },
-)
-keyboard.extensions.append(oled_ext)
 
 if __name__ == '__main__':
     keyboard.go()
